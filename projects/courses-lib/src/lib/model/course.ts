3ab9952125fb416493f5a7d85f33f2cf79449275
Cristian Rinaldi <crinaldi@santafe.gov.ar>
2020-01-31 14:30:29 -0300

    add entity support

--- projects/courses-lib/src/lib/model/course.ts
+++ projects/courses-lib/src/lib/model/course.ts
@@ -1,9 +1,10 @@
 export class Course {
 
 
-  constructor(name: string, description: string) {
+  constructor(id: number, name: string, description: string) {
     this._name = name;
     this._description = description;
+    this._id = id;
   }
 
 // tslint:disable-next-line:variable-firstName
@@ -11,6 +12,16 @@ export class Course {
 
   // tslint:disable-next-line:variable-firstName
   private _description: string;
+
+  private _id: number;
+
+  get id(): number {
+    return this._id;
+  }
+
+  set id(value: number) {
+    this._id = value;
+  }
 
   get name(): string {
     return this._name;
--- projects/courses-lib/src/lib/store/reducers/courses.reducers.ts
+++ projects/courses-lib/src/lib/store/reducers/courses.reducers.ts
@@ -1,37 +1,54 @@
 import {Course} from '../../model/course';
 import {createFeatureSelector, createReducer, createSelector, on} from '@ngrx/store';
 import {CourseActions} from '../actions';
+import {createEntityAdapter, EntityAdapter, EntityState} from "@ngrx/entity";
 
 export const coursesFeatureKey = 'courses';
 
-export interface CoursesState {
-  courses: Course[];
+export interface CoursesState extends EntityState<Course>{
+  // courses: Course[];
   loadingCourses: boolean;
   loaded: boolean;
 }
 
-const initialState: CoursesState = {
-  courses: [],
+export const adapter : EntityAdapter<Course> =
+  createEntityAdapter<Course>({
+    selectId: model => model.id
+  });
+
+
+/**
+ *  // courses: [],
+ *  // loadingCourses: false,
+ *  // loaded: false
+ */
+const initialState: CoursesState =  adapter.getInitialState({
   loadingCourses: false,
   loaded: false
-
-};
+  });
 
 export const reducer = createReducer(
   initialState,
   on(CourseActions.loadCoursesRequest, (state) => ({...state, loadingCourses: true})),
-  on(CourseActions.loadCoursesRequestSuccess, (state, {courses}) => ({...state, loadingCourses: false, courses, loaded: true}))
+  on(CourseActions.loadCoursesRequestSuccess, (state, {courses}) => (  adapter.addAll(courses, { ...state, loadingCourses: false, courses, loaded: true})))
 );
 
 export const coursesFeature = createFeatureSelector<CoursesState>(coursesFeatureKey);
 
-export const courses = createSelector(
+export const {
+  selectAll,
+  selectEntities,
+  selectIds,
+  selectTotal
+} = adapter.getSelectors();
+
+/*export const courses = createSelector(
   coursesFeature,
   (s1 => {
     console.log("Accediendo al courses", s1);
     return s1.courses
   })
-);
+);*/
 
 export const loadingCourses = createSelector(
   coursesFeature,
