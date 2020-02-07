b84cfa7f485b38364407aa104290e45e562e0dab
Cristian Rinaldi <csrinaldi@gmail.com>
2020-02-06 23:54:10 -0300

    #7

--- client/projects/chat-lib/src/lib/chat-lib.module.ts
+++ client/projects/chat-lib/src/lib/chat-lib.module.ts
@@ -4,13 +4,20 @@ import {StoreModule} from '@ngrx/store';
 import {EffectsModule} from '@ngrx/effects';
 import {ChatStore} from './store/reducer';
 import {ChatAllEffects} from './store/effects';
+import { ChatListComponent } from './components/chat-list/chat-list.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ChatListComponent],
   imports: [
     StoreModule.forFeature(ChatStore.chatFeatureKey, ChatStore.reducer),
     EffectsModule.forFeature([ChatAllEffects.ChatEffects]),
+    FlexLayoutModule,
+    MatListModule,
+    MatIconModule,
   ],
   exports: [],
   providers: [
